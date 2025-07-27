23e455e74984bac1648657f2ce7aec99da31387a
RoyAlnashef <royalnashef@gmail.com>
2025-07-27 01:20:37 -0700

    Update header border color from zinc-800 to zinc-900

--- components/header.tsx
+++ components/header.tsx
@@ -48,7 +48,7 @@ export function Header() {
   }
 
   return (
-    <header className="border-b border-zinc-800 bg-black font-sans md:relative fixed top-0 left-0 right-0 z-50">
+    <header className="border-b border-zinc-900 bg-black font-sans md:relative fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
